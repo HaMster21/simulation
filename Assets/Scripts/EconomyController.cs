0bdcbf00926a8c223fcd6af9e85c9ce9cf838fdd
HaMster21 <hamster.of.dev@gmail.com>
2013-01-03 20:59:03 +0100

    Creating the Economy controller
    The economy controller class is instanciated when the terrain is loaded and is a script component of it. The EconomyController will have to do some initialisations and is able to keep track of the data that is involved in the economical part. Its exposing members that are the global settings for the simulation.
    
    This class is only a stub now.
    
    Signed-off-by: HaMster21 <hamster.of.dev@gmail.com>

---
+++ Assets/Scripts/EconomyController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+namespace Simulation
+{
+	public class EconomyController : MonoBehaviour
+	{
+		public int CityCount;
+		public int TraderCount;
+		public int NodesPerCity;
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+	}
+}
