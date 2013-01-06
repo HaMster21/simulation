179dac6f050afd794e52124d9e4bdea112133502
HaMster21 <hamster.of.dev@gmail.com>
2013-01-06 20:02:27 +0100

    Created a class running the event loop
    This is adding a basic eventloop for the simulation to run in.

---
+++ Simulation/Simulation Controller.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace Simulation
+{
+	public class SimulationController
+	{
+		#region Fields
+		bool isRunning;
+		int updateInterval { get; set; }
+
+		System.Timers.Timer timer {get; set;}
+		int RoundCount = 1024;
+		#endregion
+
+		public static void Main (string[] args)
+		{
+			SimulationController controller = new SimulationController ();
+			controller.updateInterval = 16;
+			controller.init ();
+
+			controller.timer = new System.Timers.Timer(controller.updateInterval);
+			controller.timer.AutoReset = true;
+			controller.timer.Elapsed += new System.Timers.ElapsedEventHandler(controller.update);
+		}
+
+		public SimulationController ()
+		{
+			Console.WriteLine("New SimulationController created");
+		}
+
+		public void init ()
+		{
+			Console.WriteLine("Setting up new Simulation");
+			isRunning = true;
+		}
+
+		public void update (object source, System.Timers.ElapsedEventArgs e)
+		{
+			Console.WriteLine ("Update in progress...");
+			if (RoundCount > 0) {
+				RoundCount--;
+			}
+			else RequestshutDown();
+		}
+
+		public void RequestshutDown ()
+		{
+			isRunning = false;
+			timer.Stop();
+			timer.Dispose();
+		}
+	}
+}
+
